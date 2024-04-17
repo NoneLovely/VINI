c425d31682af98a0e4c500f4942abee55d4795dd
Ермилов Иван <150592200+NoneLovely@users.noreply.github.com>
2024-04-17 19:00:04 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 url = "https://script.google.com/macros/s/AKfycbwyEadChg5vwka_KILap939frCV7V3IwJJQGX81EOGwuhtCDiMG7P2FQ6CTzpGRbo-ZvA/exec"
 
-let tg = window.Telegram.WebApp;
+tg = window.Telegram.WebApp
 
-document.getElementById("id").value = tg.initDataUnsafe.user.id
-document.getElementById("nick").value = tg.initDataUnsafe.user.username
+id = tg.initDataUnsafe.user.id
+nick = tg.initDataUnsafe.user.username
 
-$.get(url+"?method=getAcount&acount="+tg.initDataUnsafe.user.id)
+document.getElementById("id").innerHTML = "id: " + tg.initDataUnsafe.user.id
+document.getElementById("nick").innerHTML = "ник: " + tg.initDataUnsafe.user.username
+
+$.get(url+"?method=getAcount&acount="+id)
