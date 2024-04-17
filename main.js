0eb74b17cc739335e7884ec4245d7a37406bd207
Ермилов Иван <150592200+NoneLovely@users.noreply.github.com>
2024-04-17 20:51:17 +0500

    Update main.js

--- main.js
+++ main.js
@@ -24,66 +24,68 @@ $.get(url+"?method=getToken", function (data, status){
         result = data["result"]
         
         for(let i = 0; i<result.length; i++){
-            message_text_vini = result[i]["message"]["text"]
-            message_caption_vini = result[i]["message"]["caption"]
-            message_id = result[i]["message"]["message_id"]
-            chat_id = result[i]["message"]["chat"]["id"]
+            try {
+                message_text_vini = result[i]["message"]["text"]
+                message_caption_vini = result[i]["message"]["caption"]
+                message_id = result[i]["message"]["message_id"]
+                chat_id = result[i]["message"]["chat"]["id"]
 
 
-            mc = message_caption_vini
-            mt = message_text_vini
+                mc = message_caption_vini
+                mt = message_text_vini
 
-            flag = "none"
+                flag = "none"
 
-            console.log(message_id)
-            if(mc != undefined){
-                flag = mc[0]+mc[1]+mc[2]+mc[3]+mc[4]
-                message_text_out = mc
-            }
-
-            if(mt != undefined){
-                flag = mt[0]+mt[1]+mt[2]+mt[3]+mt[4]
-                message_text_out = mt
-            }
-
-            if(flag == "#vini" & chat_id == id){
-                data_el = document.createElement("div");
-                
-                data_el.innerHTML = `
-                    <div class="px-10 mt-10 py-2.5 bg-black bg-opacity-25 shadow-2xl rounded-md items-center">
-                        <p>
-                            `+ message_text_out +`
-                        </p>
-                    </div>
-
-                    <button id="btnsend`+i+`" class="w-full mt-5 py-2.5 bg-black bg-opacity-25 shadow-2xl rounded-md items-center hover:bg-opacity-15">
-                        отправить
-                    </button>
-                `
-                                    
-                $("#workspace")[0].appendChild(data_el)
-                document.write = document._write
-
-                $("#btnsend"+i)[0].onclick = function (){
-                    $.get(url+"?method=getToken", function (data, status){
-                        token = data["ViniAPI"]
-                
-                        Http = new XMLHttpRequest()
-                
-                        data = {
-                            "chat_id": chanel,
-                            "from_chat_id": id,
-                            "message_id": result[i]["message"]["message_id"]
-                        }
-
-                        if(acount == "активирован"){
-                            Http.open("POST", bot_url+token+"/forwardMessage")
-                            Http.setRequestHeader("Content-Type", "application/json")
-                            Http.send(JSON.stringify(data))
-                        }
-                    })
+                console.log(message_id)
+                if(mc != undefined){
+                    flag = mc[0]+mc[1]+mc[2]+mc[3]+mc[4]
+                    message_text_out = mc
                 }
-            }
+
+                if(mt != undefined){
+                    flag = mt[0]+mt[1]+mt[2]+mt[3]+mt[4]
+                    message_text_out = mt
+                }
+
+                if(flag == "#vini" & chat_id == id){
+                    data_el = document.createElement("div");
+                    
+                    data_el.innerHTML = `
+                        <div class="px-10 mt-10 py-2.5 bg-black bg-opacity-25 shadow-2xl rounded-md items-center">
+                            <p>
+                                `+ message_text_out +`
+                            </p>
+                        </div>
+
+                        <button id="btnsend`+i+`" class="w-full mt-5 py-2.5 bg-black bg-opacity-25 shadow-2xl rounded-md items-center hover:bg-opacity-15">
+                            отправить
+                        </button>
+                    `
+                                        
+                    $("#workspace")[0].appendChild(data_el)
+                    document.write = document._write
+
+                    $("#btnsend"+i)[0].onclick = function (){
+                        $.get(url+"?method=getToken", function (data, status){
+                            token = data["ViniAPI"]
+                    
+                            Http = new XMLHttpRequest()
+                    
+                            data = {
+                                "chat_id": chanel,
+                                "from_chat_id": id,
+                                "message_id": result[i]["message"]["message_id"]
+                            }
+
+                            if(acount == "активирован"){
+                                Http.open("POST", bot_url+token+"/forwardMessage")
+                                Http.setRequestHeader("Content-Type", "application/json")
+                                Http.send(JSON.stringify(data))
+                            }
+                        })
+                    }
+                }
+            } catch (err) {}
         }
     })
 })
